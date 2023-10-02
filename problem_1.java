228c1504ba1b47afe9ffbb904a766c6712951c6c
S <s>
2023-10-03 03:16:52 +0400

    {"types":"C","runs":3}

--- problem_1.java
+++ problem_1.java
@@ -2,6 +2,6 @@ import stanford.karel.*;
 
 public class problem_1 extends SuperKarel{
 	public static void main(String[] args) {
-	    System.out.println("Hello World");
-	 }
+	   System.out.println("Hello World");
+	}
 }
