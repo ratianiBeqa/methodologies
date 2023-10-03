efa099a9019077cbbaf72ce836575d8fb51610c4
S <s>
2023-10-03 18:23:35 +0400

    {"types":"C","runs":0}

--- Seminar03.java
+++ Seminar03.java
@@ -1,4 +1,12 @@
+import stanford.karel.SuperKarel;
 
-public class Seminar03 {
-
+public class Seminar03 extends SuperKarel {
+	public void run(){
+		move();
+		move();
+	}
+	
+	public void buildHouse(){
+		
+	}
 }
