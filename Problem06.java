8078c90fe57a756a5646b019602440130f8bc567
S <s>
2023-10-03 04:38:32 +0400

    {"types":"C","runs":2}

--- Problem06.java
+++ Problem06.java
@@ -9,6 +9,11 @@ public class Problem06 extends SuperKarel {
 			pickBeeper();
 		}
 		move();
+		while(beepersPresent()){
+			beeperCount ++;
+			pickBeeper();
+		}
+		move();
 		for (int i=0; i<beeperCount; i++) {
 			putBeeper();
 		}
