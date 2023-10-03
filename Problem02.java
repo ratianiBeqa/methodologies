53a5bbad482ea7dad56989cf9cfa3a284c636059
S <s>
2023-10-03 04:02:05 +0400

    {"types":"C","runs":2}

--- Problem02.java
+++ Problem02.java
@@ -1,4 +1,17 @@
+import stanford.karel.SuperKarel;
 
-public class Problem02 {
-
+public class Problem02 extends SuperKarel {
+	public void run() {
+		move();
+		move();
+		move();
+		move();
+		for (int i=0; i<10; i++) {
+			pickBeeper();
+		}
+		move();
+		for (int i=0; i<10; i++) {
+			putBeeper();
+		}
+	}
 }
