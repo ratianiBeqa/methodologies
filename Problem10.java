eb11316c874111cba41bd779982f897629c43cb5
S <s>
2023-10-05 14:46:12 +0400

    {"types":"C","runs":1}

--- Problem10.java
+++ Problem10.java
@@ -4,6 +4,40 @@ public class Problem10 extends Karel {
 
 	public void run() {
 		move();
+		divide();
+	}
+	
+	private void divide() {
+		while(beepersPresent()) {
+			move();
+			while(beepersPresent()) {
+				pickBeeper();
+				turnAround();
+				move();
+				pickBeeper();
+				turnAround();
+				move();
+				move();
+				move();
+				putBeeper();
+				move();
+				putBeeper();
+				resetPosition();
+			}	
+		}
+	}
+	
+	private void resetPosition() {
+		turnAround();
+		move();
+		move();
+		move();
+		turnAround();
+	}
+	
+	private void turnAround() {
+		turnLeft();
+		turnLeft();
 	}
 	
 }
