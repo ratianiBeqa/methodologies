c661d137a8e11953378e7c3f6412b49a3ca6626f
S <s>
2023-10-05 13:16:27 +0400

    {"types":"A","runs":8}

---
+++ Problem09.java
@@ -0,0 +1,45 @@
+import stanford.karel.Karel;
+
+public class Problem09 extends Karel {
+
+	public void run() {
+		move();
+		move();
+		takeAway();
+		moveBeepersToDest();
+	}
+	
+	private void takeAway() {
+		while(beepersPresent()) {
+			turnAround();
+			pickBeeper();
+			move();
+			pickBeeper();
+			move();
+			putBeeper();
+			turnAround();
+			move();
+			move();
+		}
+	}
+	
+	private void moveBeepersToDest() {
+		turnAround();
+		move();
+		while(beepersPresent()) {
+			pickBeeper();
+			turnAround();
+			move();
+			move();
+			putBeeper();
+			turnAround();
+			move();
+			move();
+		}
+	}
+	
+	private void turnAround() {
+		turnLeft();
+		turnLeft();
+	}
+}
