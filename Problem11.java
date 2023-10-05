68a82d74a90237b28a4102070329c92d617446b2
S <s>
2023-10-05 14:54:43 +0400

    {"types":"A","runs":5}

---
+++ Problem11.java
@@ -0,0 +1,69 @@
+import stanford.karel.Karel;
+
+public class Problem11 extends Karel {
+
+	public void run() {
+		move();
+		divide();
+	}
+	
+	private void divide() {
+		while(beepersPresent()) {
+			move();
+			while(beepersPresent()) {
+				pickBeeper();
+				turnAround();
+				move();
+				pickBeeper();
+				turnAround();
+				move();
+				move();
+				move();
+				putBeeper();
+				move();
+				putBeeper();
+				resetPosition();
+			}
+			move();
+			putBeeper();
+			returnDivider();
+			resetInitialPosition();
+		}
+	}
+	
+	private void returnDivider() {
+		move();
+		while(beepersPresent()) {
+			turnAround();
+			pickBeeper();
+			move();
+			move();
+			putBeeper();
+			turnAround();
+			move();
+			move();
+		}
+	}
+	
+	private void resetPosition() {
+		turnAround();
+		move();
+		move();
+		move();
+		turnAround();
+	}
+	
+	private void resetInitialPosition() {
+		turnAround();
+		move();
+		move();
+		move();
+		turnAround();
+	}
+	
+	private void turnAround() {
+		turnLeft();
+		turnLeft();
+	}
+	
+}
