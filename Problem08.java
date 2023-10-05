0f17f5410bd249fad61b0676c2fe888559d1bd1f
S <s>
2023-10-05 13:11:20 +0400

    {"types":"C","runs":0}

--- Problem08.java
+++ Problem08.java
@@ -1,43 +1,27 @@
-import stanford.karel.SuperKarel;
+import stanford.karel.Karel;
 
-public class Problem08 extends SuperKarel {
-
-	boolean filled = false;
+public class Problem08 extends Karel {
 
 	public void run() {
-		while(leftIsClear()){
-			fillRow();
-			resetPosition();
-			goUp();
-		}
-		fillRow();
-	}
-	public void fillRow() {
-		if(noBeepersPresent()) {
-			putBeeper();
-		}
-		while(frontIsClear()) {
-			move();
-			if(noBeepersPresent()) {
-				putBeeper();
-			}
-		}
-	}
-	
-	public void resetPosition() {
-		turnLeft();
-		turnLeft();
-		while(frontIsClear()) {
-			move();
-		}
-		turnLeft();
-		turnLeft();
-	}
-	
-	public void goUp() {
-		turnLeft();
 		move();
-		turnLeft();
+		move();
+		takeAway();
+	}
+	
+	private void takeAway() {
+		while(beepersPresent()) {
+			turnAround();
+			move();
+			pickBeeper();
+			move();
+			putBeeper();
+			turnAround();
+			move();
+			move();
+		}
+	}
+	
+	private void turnAround() {
 		turnLeft();
 		turnLeft();
 	}
