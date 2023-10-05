6d7340813f8204cfee3f08abe46a007c853e70ef
S <s>
2023-10-05 15:19:17 +0400

    {"types":"C","runs":18}

--- Problem11.java
+++ Problem11.java
@@ -11,7 +11,7 @@ public class Problem11 extends Karel {
 			if(beepersPresent()) {
 				pickBeeper();
 			}
-			if(rightIsClear()) {
+			while(rightIsClear()) {
 				turnLeft();
 				turnLeft();
 				turnLeft();
