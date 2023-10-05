6209c6b02c62b3017701eef8063a9c0cadecbc2b
S <s>
2023-10-05 14:55:04 +0400

    {"types":"C","runs":5}

--- Problem11.java
+++ Problem11.java
@@ -4,66 +4,6 @@ public class Problem11 extends Karel {
 
 	public void run() {
 		move();
-		divide();
-	}
-	
-	private void divide() {
-		while(beepersPresent()) {
-			move();
-			while(beepersPresent()) {
-				pickBeeper();
-				turnAround();
-				move();
-				pickBeeper();
-				turnAround();
-				move();
-				move();
-				move();
-				putBeeper();
-				move();
-				putBeeper();
-				resetPosition();
-			}
-			move();
-			putBeeper();
-			returnDivider();
-			resetInitialPosition();
-		}
-	}
-	
-	private void returnDivider() {
-		move();
-		while(beepersPresent()) {
-			turnAround();
-			pickBeeper();
-			move();
-			move();
-			putBeeper();
-			turnAround();
-			move();
-			move();
-		}
-	}
-	
-	private void resetPosition() {
-		turnAround();
-		move();
-		move();
-		move();
-		turnAround();
-	}
-	
-	private void resetInitialPosition() {
-		turnAround();
-		move();
-		move();
-		move();
-		turnAround();
-	}
-	
-	private void turnAround() {
-		turnLeft();
-		turnLeft();
 	}
 	
 }
