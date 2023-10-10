2a623f2c36203496869bf96e45fc8ad4831894da
S <s>
2023-10-10 21:55:07 +0400

    {"types":"C","runs":33}

--- Problem12.java
+++ Problem12.java
@@ -11,16 +11,16 @@ public class Problem12 extends GraphicsProgram {
 		GRect door = new GRect(50, 100);
 		add(door, 150+75, height-100);
 		
-		GLine roofLine = new GLine(150, 200, 150+100, 60);
+		GLine roofLine = new GLine(150, 200, 150+100, height-200);
 		add(roofLine);
 		
-		GLine roofLine2 = new GLine(150+100, 60, 200+150, 200);
+		GLine roofLine2 = new GLine(150+100, 60, 200+150, height-200);
 		add(roofLine2);
 		
 		GRect windowFirst = new GRect(50, 50);
-		add(windowFirst, 175, 225);
+		add(windowFirst, 175, height - 175);
 		GRect windowSecond = new GRect(50, 50);
-		add(windowSecond, 275, 225);
+		add(windowSecond, 275, height - 175);
 		
 	}
 }
