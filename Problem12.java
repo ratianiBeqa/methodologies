28db9ddfe77ea199cda2137c7f324890dd6be63d
S <s>
2023-10-10 18:57:24 +0400

    {"types":"C","runs":8}

--- Problem12.java
+++ Problem12.java
@@ -4,6 +4,6 @@ import acm.program.GraphicsProgram;
 public class Problem12 extends GraphicsProgram {
 	public void run() {
 		GRect myRect = new GRect(200, 200);
-		add(myRect, 100, 100);
+		add(myRect, 150, 200);
 	}
 }
