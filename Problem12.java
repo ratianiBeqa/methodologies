f19981ad60842a38edaf554d694241d8e3dfe35c
S <s>
2023-10-10 22:12:53 +0400

    {"types":"C","runs":49}

--- Problem12.java
+++ Problem12.java
@@ -9,7 +9,7 @@ public class Problem12 extends GraphicsProgram {
 		GRect myRect = new GRect(200, 200);
 		add(myRect, 150, height-200);
 		myRect.setFilled(true);
-		myRect.setFillColor(Color.ORANGE);
+		myRect.setFillColor(Color.orange);
 		
 		GRect door = new GRect(50, 100);
 		add(door, 150+75, height-80);
