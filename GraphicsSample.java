df41b6d9d5aa3c4017c9520a28c9ddc770fdca69
S <s>
2023-10-10 18:27:47 +0400

    {"types":"AR","runs":0}

---
+++ GraphicsSample.java
@@ -0,0 +1,8 @@
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
+
+public class GraphicsSample extends GraphicsProgram {
+	public void run() {
+		GRect myRect = new GRect(100, 200);
+	}
+}
