bd157ed9c8b437600929b487997aff29808dbe05
S <s>
2023-10-17 16:01:10 +0400

    {"types":"C","runs":0}

--- Problem13.java
+++ Problem13.java
@@ -2,7 +2,7 @@ import acm.graphics.*;
 import acm.program.GraphicsProgram;
 
 public class Problem13 extends GraphicsProgram {
-	public void run() {			
+	public void run() {
 		int w = getWidth();
 		int h = getHeight();
 		GLine horizontal = new GLine(0, h/2, w, h/2);
