808d44c26326dd4e52d73b4dff4ca06aaa795d96
S <s>
2023-10-17 16:57:14 +0400

    {"types":"C","runs":28}

--- Problem16.java
+++ Problem16.java
@@ -1,38 +1,10 @@
-import acm.graphics.*;
-import acm.program.GraphicsProgram;
-import java.awt.Color;
+import acm.program.ConsoleProgram;
 
-public class Problem16 extends GraphicsProgram {
+public class Problem16 extends ConsoleProgram {
 	public void run() {
-		int w = getWidth();
-		int h = getHeight();
-		
-		GRect head = new GRect(100, 100);
-		add(head, w/2 - 50, h/2 - 50);
-		head.setFilled(true);
-	    head.setColor(Color.orange);
-		
-		GLine antanaOne = new GLine(w/2 - 50, h/2 - 50, w/2 - 50, h/2 - 100);
-		GLine antanaTwo = new GLine(w/2 + 50, h/2 - 50, w/2 + 50, h/2 - 100);
-		add(antanaOne);
-		add(antanaTwo);
-		
-		GLine eyeOne = new GLine(w/2 - 30, h/2 - 30, w/2 - 10, h/2 - 10);
-		GLine eyeTwo = new GLine(w/2 + 10, h/2 - 10, w/2 + 30, h/2 - 30);
-		add(eyeOne);
-		add(eyeTwo);
-		
-		GRect mouth = new GRect(70, 20);
-		mouth.setFilled(true);
-		mouth.setColor(Color.white);
-		add(mouth, w/2 - 35, h/2 + 20);
-
-		GLine toothOne = new GLine(w/2 - 35 + 70/3, h/2 + 20, w/2 - 35 + 70/3, h/2 + 40);
-		toothOne.setColor(Color.LIGHT_GRAY);
-		add(toothOne);
-		
-		GLine toothTwo = new GLine(w/2 - 35 + 140/3, h/2 + 20, w/2 - 35 + 140/3, h/2 + 40);
-		toothTwo.setColor(Color.LIGHT_GRAY);
-		add(toothTwo);
+		int a = readInt("First number: ");
+		int b = readInt("Second number: ");
+		int c = readInt("Third number: ");
+		println(1 / (1/a + 1/b + 1/c));
 	}
 }
