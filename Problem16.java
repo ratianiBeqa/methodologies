5bcb9c6a497e2d0938c35b161d05de770d8b5159
S <s>
2023-10-17 17:02:51 +0400

    {"types":"C","runs":34}

--- Problem16.java
+++ Problem16.java
@@ -2,6 +2,7 @@ import acm.program.ConsoleProgram;
 
 public class Problem16 extends ConsoleProgram {
 	public void run() {
+		//pirobashi mtel ricxvebze ki weria mara qrashavs mase
 		double a = readDouble("First number: ");
 		double b = readDouble("Second number: ");
 		double c = readDouble("Third number: ");
