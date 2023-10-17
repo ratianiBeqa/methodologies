6e66422cd2105465641d5b7047af3a94297fd5d0
S <s>
2023-10-17 17:25:59 +0400

    {"types":"C","runs":37}

--- Problem18.java
+++ Problem18.java
@@ -2,11 +2,12 @@ import acm.program.ConsoleProgram;
 
 public class Problem18 extends ConsoleProgram {
 	public void run() {
-		double num = readDouble("enter number: ");
-		int intPart = (int) num;
-		double fractPart = num - intPart;
-
-		println("int part: " + intPart);
-		println("fract part: " + fractPart);
+		int a = readInt("Enter num 1: ");
+		int b = readInt("Enter num 2: ");
+		if (a > b) {
+			println(a);
+		} else {
+			println(b);
+		}
 	}
 }
