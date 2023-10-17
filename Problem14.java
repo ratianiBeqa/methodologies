c181cb45209e5ca7dd8ed0498dbbf3e75c118acf
S <s>
2023-10-17 16:34:18 +0400

    {"types":"C","runs":25}

--- Problem14.java
+++ Problem14.java
@@ -26,7 +26,7 @@ public class Problem14 extends GraphicsProgram {
 		mouth.setFilled(true);
 		mouth.setColor(Color.white);
 		add(mouth, w/2 - 35, h/2 + 20);
-
+		
 		GLine toothOne = new GLine(w/2 - 35 + 70/3, h/2 + 20, w/2 - 35 + 70/3, h/2 + 40);
 		toothOne.setColor(Color.LIGHT_GRAY);
 		add(toothOne);
