ef11bffa8c569a02549d864fe46b7a6e8723ba83
S <s>
2023-10-17 17:05:49 +0400

    {"types":"C","runs":34}

--- Problem13.java
+++ Problem13.java
@@ -5,7 +5,7 @@ public class Problem13 extends GraphicsProgram {
 	public void run() {
 		int w = getWidth();
 		int h = getHeight();
-		GLine horizontal = new GLine(0, h/2, w, h/2);
+		GLine horizontal = new GLine(0, h / 2, w, h / 2);
 		add(horizontal);
 	}
 }
