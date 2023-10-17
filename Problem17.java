1cb27df206b21d514b525a690fff9b43e5f68647
S <s>
2023-10-17 17:07:30 +0400

    {"types":"C","runs":35}

--- Problem17.java
+++ Problem17.java
@@ -3,6 +3,6 @@ import acm.program.ConsoleProgram;
 public class Problem17 extends ConsoleProgram {
 	public void run() {
 		double num = readDouble("enter number: ");
-		
+		println(int(num));
 	}
 }
