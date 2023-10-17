09a6cc5411f30f2de30a19a0431fa2da5880766f
S <s>
2023-10-17 17:27:53 +0400

    {"types":"A","runs":40}

---
+++ Problem19.java
@@ -0,0 +1,15 @@
+import acm.program.ConsoleProgram;
+
+public class Problem19 extends ConsoleProgram {
+	public void run() {
+		int a = readInt("Enter num 1: ");
+		int b = readInt("Enter num 2: ");
+		if (a > b) {
+			println("Max is: " + a);
+		} else if (a < b){
+			println("Max is: " + b);
+		} else {
+			println("They are equal");
+		}
+	}
+}
