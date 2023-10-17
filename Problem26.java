31b2d0b964c34808b9cd274a3633ddd34fee6ae0
S <s>
2023-10-17 17:43:39 +0400

    {"types":"A","runs":52}

---
+++ Problem26.java
@@ -0,0 +1,21 @@
+import acm.program.ConsoleProgram;
+
+public class Problem26 extends ConsoleProgram {
+	public void run() {
+		int a = readInt("Enter num 1: ");
+		int b = readInt("Enter num 2: ");
+		int min;
+		if (a > b) {
+			min = b;
+		} else {
+			min = a;
+		}
+		
+		for (int i=min; i>=1; i--) {
+			if (b % i == 0 && a % i == 0) {
+				println("USG: " + i);
+				break;
+			}
+		}
+	}
+}
