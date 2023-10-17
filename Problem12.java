ff30daa4801109ce5ab6fae4bd84074cea0baa3b
S <s>
2023-10-17 17:05:52 +0400

    {"types":"C","runs":34}

--- Problem12.java
+++ Problem12.java
@@ -5,23 +5,23 @@ import java.awt.Color;
 public class Problem12 extends GraphicsProgram {
 	public void run() {
 		int height = getHeight();
-		
+
 		GRect myRect = new GRect(200, 200);
-		add(myRect, 150, height-200);
+		add(myRect, 150, height - 200);
 		myRect.setFilled(true);
 		myRect.setFillColor(new Color(150, 75, 0));
-		
+
 		GRect door = new GRect(50, 100);
-		add(door, 150+75, height-80);
+		add(door, 150 + 75, height - 80);
 		door.setFilled(true);
 		door.setColor(Color.orange);
-		
-		GLine roofLine = new GLine(150, height-200, 150+100, height-300);
+
+		GLine roofLine = new GLine(150, height - 200, 150 + 100, height - 300);
 		add(roofLine);
-		
-		GLine roofLine2 = new GLine(150+200, height-200, 150+100, height-300);
+
+		GLine roofLine2 = new GLine(150 + 200, height - 200, 150 + 100, height - 300);
 		add(roofLine2);
-		
+
 		GRect windowFirst = new GRect(50, 50);
 		add(windowFirst, 175, height - 175);
 		windowFirst.setFilled(true);
@@ -31,10 +31,10 @@ public class Problem12 extends GraphicsProgram {
 		add(windowSecond, 275, height - 175);
 		windowSecond.setFilled(true);
 		windowSecond.setFillColor(Color.cyan);
-		
-		GOval doorKnob = new GOval(240, height-30, 5, 5);
+
+		GOval doorKnob = new GOval(240, height - 30, 5, 5);
 		add(doorKnob);
 		doorKnob.setFilled(true);
-		
+
 	}
 }
