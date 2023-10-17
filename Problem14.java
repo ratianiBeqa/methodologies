ac300a3f98dde555f70e5936ece19281f94867da
S <s>
2023-10-17 16:32:52 +0400

    {"types":"C","runs":25}

--- Problem14.java
+++ Problem14.java
@@ -34,35 +34,5 @@ public class Problem14 extends GraphicsProgram {
 		GLine toothTwo = new GLine(w/2 - 35 + 140/3, h/2 + 20, w/2 - 35 + 140/3, h/2 + 40);
 		toothTwo.setColor(Color.LIGHT_GRAY);
 		add(toothTwo);
-		
-
-//		myRect.setFilled(true);
-//		myRect.setFillColor(new Color(150, 75, 0));
-//		
-//		GRect door = new GRect(50, 100);
-//		add(door, 150+75, height-80);
-//		door.setFilled(true);
-//		door.setColor(Color.orange);
-//		
-//		GLine roofLine = new GLine(150, height-200, 150+100, height-300);
-//		add(roofLine);
-//		
-//		GLine roofLine2 = new GLine(150+200, height-200, 150+100, height-300);
-//		add(roofLine2);
-//		
-//		GRect windowFirst = new GRect(50, 50);
-//		add(windowFirst, 175, height - 175);
-//		windowFirst.setFilled(true);
-//		windowFirst.setFillColor(Color.cyan);
-//
-//		GRect windowSecond = new GRect(50, 50);
-//		add(windowSecond, 275, height - 175);
-//		windowSecond.setFilled(true);
-//		windowSecond.setFillColor(Color.cyan);
-//		
-//		GOval doorKnob = new GOval(240, height-30, 5, 5);
-//		add(doorKnob);
-//		doorKnob.setFilled(true);
-//		
 	}
 }
