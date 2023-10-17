d36c972f940382afdab85d6468017066c9c7f76f
S <s>
2023-10-17 16:26:03 +0400

    {"types":"C","runs":20}

--- Problem14.java
+++ Problem14.java
@@ -22,8 +22,10 @@ public class Problem14 extends GraphicsProgram {
 		
 		GRect mouth = new GRect(70, 20);
 		add(mouth, w/2 - 35, h/2 + 20);
-		GLine tooth = new GLine(w/2 - 35 + 70 / 3, h/2 + 20, w/2 - 35 + 70 / 3, h/2 + 40);
-		add(tooth);
+		GLine toothOne = new GLine(w/2 - 35 + 70 / 3, h/2 + 20, w/2 - 35 + 70 / 3, h/2 + 40);
+		GLine toothTwo = new GLine(w/2 - 35 + 140 / 3, h/2 + 20, w/2 - 35 + 140 / 3, h/2 + 40);
+		add(toothOne);
+		add(toothTwo);
 		
 
 //		myRect.setFilled(true);
